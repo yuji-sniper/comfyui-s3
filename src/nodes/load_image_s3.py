f70b5ecd2bb99de4eb17bda172226e7703361b48
吉村直人 <ahowateto123@gmail.com>
2025-10-12 00:35:45 +0900

    load_image_s3: Use context manager for opening images to ensure proper resource management

--- src/nodes/load_image_s3.py
+++ src/nodes/load_image_s3.py
@@ -28,30 +28,30 @@ class LoadImageS3:
         image_path = S3_INSTANCE.download_file(s3_path=s3_path, local_path=f"input/{image}")
         
         try:
-            img = Image.open(image_path)
-            output_images = []
-            output_masks = []
-            for i in ImageSequence.Iterator(img):
-                i = ImageOps.exif_transpose(i)
-                if i.mode == 'I':
-                    i = i.point(lambda i: i * (1 / 255))
-                image = i.convert("RGB")
-                image = np.array(image).astype(np.float32) / 255.0
-                image = torch.from_numpy(image)[None,]
-                if 'A' in i.getbands():
-                    mask = np.array(i.getchannel('A')).astype(np.float32) / 255.0
-                    mask = 1. - torch.from_numpy(mask)
-                else:
-                    mask = torch.zeros((64,64), dtype=torch.float32, device="cpu")
-                output_images.append(image)
-                output_masks.append(mask.unsqueeze(0))
+            with Image.open(image_path) as img:
+                output_images = []
+                output_masks = []
+                for i in ImageSequence.Iterator(img):
+                    i = ImageOps.exif_transpose(i)
+                    if i.mode == 'I':
+                        i = i.point(lambda i: i * (1 / 255))
+                    image = i.convert("RGB")
+                    image = np.array(image).astype(np.float32) / 255.0
+                    image = torch.from_numpy(image)[None,]
+                    if 'A' in i.getbands():
+                        mask = np.array(i.getchannel('A')).astype(np.float32) / 255.0
+                        mask = 1. - torch.from_numpy(mask)
+                    else:
+                        mask = torch.zeros((64,64), dtype=torch.float32, device="cpu")
+                    output_images.append(image)
+                    output_masks.append(mask.unsqueeze(0))
 
-            if len(output_images) > 1:
-                output_image = torch.cat(output_images, dim=0)
-                output_mask = torch.cat(output_masks, dim=0)
-            else:
-                output_image = output_images[0]
-                output_mask = output_masks[0]
+                if len(output_images) > 1:
+                    output_image = torch.cat(output_images, dim=0)
+                    output_mask = torch.cat(output_masks, dim=0)
+                else:
+                    output_image = output_images[0]
+                    output_mask = output_masks[0]
         finally:
             os.remove(image_path)
 
